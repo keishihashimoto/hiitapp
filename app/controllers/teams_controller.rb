323ab9aff2347f65774a05b8f17eaa9f61e353df
keishihashimoto <keishi04hrikzira@yahoo.co.jp>
2021-08-31 17:50:26 +0900

    チーム登録機能の実装

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,8 +5,16 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
   def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to root_path # 後でサインアップページに変更
+    else
+      render :new
+    end
   end
 
   private
-  def
+  def team_params
+    params.require(:team).permit(:name)
+  end
 end
