101359ecb5113f9d9fe5cd70eaa745f52cebe22b
keishihashimoto <keishi04hrikzira@yahoo.co.jp>
2021-09-01 16:18:47 +0900

    ユーザー削除機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
 class UsersController < ApplicationController
-  before_action :set_team
+  before_action :set_team, only: [:index]
+  before_action :set_user, only: [:index, :destroy]
   before_action :admin_user?, only: [:index]
   def index
-    @user = User.find(params[:id])
   end
 
+  def destroy
+  @user.destroy
+  redirect_to root_path
+  end
   private
   def set_team
     @team = current_user.team
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
   def admin_user?
