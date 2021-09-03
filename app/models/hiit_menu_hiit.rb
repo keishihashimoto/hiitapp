8dd4885ef77838e15e51b56cae705c06633976f1
keishihashimoto <keishi04hrikzira@yahoo.co.jp>
2021-09-03 11:41:48 +0900

    HIIT登録画面にプロンプトが表示されるように修正

--- app/models/hiit_menu_hiit.rb
+++ app/models/hiit_menu_hiit.rb
@@ -7,8 +7,8 @@ class HiitMenuHiit
     validates :date
     validates :menu_ids
     validates :team_id
-    validates :active_time, format: { with: /\A[0-9]{2}[分秒]\z/ }
-    validates :rest_time, format: { with: /\A[0-9]{2}[分秒]\z/ }
+    validates :active_time, format: { with: /\A[0-9]{1,2}[分秒]\z/ }
+    validates :rest_time, format: { with: /\A[0-9]{1,2}[分秒]\z/ }
   end
 
   def save
@@ -21,9 +21,9 @@ class HiitMenuHiit
     MenuHiit.create(hiit_id: hiit.id, menu_id: menu_ids[5])
     MenuHiit.create(hiit_id: hiit.id, menu_id: menu_ids[6])
     MenuHiit.create(hiit_id: hiit.id, menu_id: menu_ids[7])
-    date_count = date.length - 1 # 配列の中の""をエスケープ
+    date_count = date.length
     date_count.times do |i|
-    HiitDate.create(hiit_id: hiit.id, date: date[(i+1)])
+    HiitDate.create(hiit_id: hiit.id, date: date[i])
     end
   end
 
