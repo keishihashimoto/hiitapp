9d151b45e66c8148b833138fd7a1212751948c31
keishihashimoto <keishi04hrikzira@yahoo.co.jp>
2021-09-03 12:05:51 +0900

    バリデーションの追加

--- app/models/hiit_date.rb
+++ app/models/hiit_date.rb
@@ -1,5 +1,4 @@
 class HiitDate < ApplicationRecord
-  validates :date, null: false, numericality: { euqal_to_or_more_than: 0, euqal_to_or_less_than: 6 }
   belongs_to :hiit
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :wday
--- app/models/hiit_menu_hiit.rb
+++ app/models/hiit_menu_hiit.rb
@@ -10,6 +10,8 @@ class HiitMenuHiit
     validates :active_time, format: { with: /\A[0-9]{1,2}[分秒]\z/ }
     validates :rest_time, format: { with: /\A[0-9]{1,2}[分秒]\z/ }
   end
+  validate :collect_date?
+  validate :eight_menus?
 
   def save
     hiit = Hiit.create(name: name, active_time: active_time, rest_time: rest_time, team_id: team_id)
@@ -25,6 +27,31 @@ class HiitMenuHiit
     date_count.times do |i|
     HiitDate.create(hiit_id: hiit.id, date: date[i])
     end
+  end
+
+  # menu_idsが8つのmenu_idを持っているかどうかを判定するバリデーションメソッド
+  def eight_menus?
+    if menu_ids != [] || menu_ids.length != 8 || menu_ids.include?("")
+      errors.add(:menu_ids, "must be choosen at eight-times")
+    end
+  end
+
+  # dateが0~6以外の数字を持っていないかどうか、また要素の数が８つ以上になっていないかどうかを判定するバリデーションメソッド
+  def collect_date?
+
+    if date = []
+      if date.length >= 8
+        errors.add(:date, "is invalid")
+      else
+        date.each do |d|
+          if d >= 7 || d < 0
+            errors.add(:date, "is invalid")
+            return
+          end
+        end
+      end
+    end
+
   end
 
 end
